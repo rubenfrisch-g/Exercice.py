8575db82e6a2f8259bde216fc6ae984ad342ed1d
Ruben Frisch <rubenfrisch@Rubens-MacBook-Pro-7.local>
2026-02-09 12:18:32 +0200

    first commit

---
+++ Crash-Course/(1)Exercie.py
@@ -0,0 +1,27 @@
+first = "Hello World"
+#This is a comment.
+print("I AM A COMPUTER!")
+if 1 < 2 and 4 > 2 :
+    print("Math is Fun")
+nope = None
+True and False
+print(len("What's my length?"))
+print("i am shouting".upper())
+print(int("1000"))
+print("4"+"real")
+print(3 * "cool")
+print(1 / 0)
+type([])
+name = input("What is your name ?")
+number = int(input("Chose a random number"))
+if number < 0 :
+    print("That number is less than 0!")
+if number > 0 :
+    print("That number is greater than 0!")
+if number == 0 :
+    print("You picked 0!")
+print("apple".find("l"))
+if int(("xylophne".find("y"))) > 0 :
+    print("YEAHHHH y is in the word ")
+print("my_string".islower())
+print('angry')
---
+++ Crash-Course/(2)Exercice.py
@@ -0,0 +1,19 @@
+humanYears >= int(0)
+catYears = int
+print(input("how old are you"))
+
+def calculate_years(humanYears):
+    if humanYears == int(1):
+        catYears == int(15)
+    if humanYears == int(2):
+        catYears == int(24)
+    else catYears == int(humanYears + int(4))
+
+
+
+print(calculate_years)
+
+
+
+
+
