8ea759effb1f98bf8c252068bbb5efd34197e4d1
Ruben Frisch <rubenfrisch@Rubens-MacBook-Pro-7.local>
2026-02-09 14:42:19 +0200

    initial commit

--- Crash-Course/(2)Exercice.py
+++ Crash-Course/(2)Exercice.py
@@ -1,19 +1,25 @@
-humanYears >= int(0)
-catYears = int
-print(input("how old are you"))
+# cat_Years = "age of cat"
+# dog_Years = "age of dog"
+# human_years = "age of human"
 
-def calculate_years(humanYears):
-    if humanYears == int(1):
-        catYears == int(15)
-    if humanYears == int(2):
-        catYears == int(24)
-    else catYears == int(humanYears + int(4))
+def calculate_years(human_Years):
+    if human_Years == int(1):
+        cat_Years = int(15) 
+        dog_Years = int(15)
+        # print(cat_Years, dog_Years)
+    elif human_Years == int(2):
+        cat_Years = int(24)
+        dog_Years = int(24)
+        # print(cat_Years, dog_Years)
+    else:
+        cat_Years = 24 + (human_Years - 2) * 4
+        dog_Years = 24 + (human_Years - 2) * 5
+        # print(cat_Years, dog_Years)
+        
 
+    return [human_Years, cat_Years, dog_Years]
 
-
-print(calculate_years)
-
-
-
-
+print(calculate_years(int(10)))
+print(calculate_years(int(1)))
+print(calculate_years(int(2)))
 
