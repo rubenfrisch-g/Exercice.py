9f70ecec95592de6313ad6dd2113d04f8d4ac8a1
Ruben Frisch <rubenfrisch@Rubens-MacBook-Pro-7.local>
2026-02-16 15:09:04 +0200

    heyhey

--- Crash-Course/Exercice(dictonary).py
+++ Crash-Course/Exercice(dictonary).py
@@ -1,26 +1,34 @@
-# Exercie 1
+# # Exercie 1
 
-pairs = [("name", "Elie"), ("job", "Instructor")]
-print(dict(pairs))
+# pairs = [("name", "Elie"), ("job", "Instructor")]
+# print(dict(pairs))
 
-# Exercice 2
+# # Exercice 2
 
-init = ["CA", "NJ", "RI"]
-full_name = ["California", "New Jersey", "Rhode Island"]
-print(dict(zip(init, full_name)))
+# init = ["CA", "NJ", "RI"]
+# full_name = ["California", "New Jersey", "Rhode Island"]
+# print(dict(zip(init, full_name)))
 
-# Exercice 3
+# # Exercice 3
 
-vowel_dict = {X : 0 for X in "aeiouY"}
-print(vowel_dict)
+# vowel_dict = {X : 0 for X in "aeiouY"}
+# print(vowel_dict)
 
-#  Exercie 4
+# #  Exercie 4
 
-alphabet_dict = {i: chr(65 + i) for i in range(26)}
-print({i: chr(65 + i) for i in range(26)})
+# alphabet_dict = {i: chr(65 + i) for i in range(26)}
+# print({i: chr(65 + i) for i in range(26)})
 
-#  Exercie 5 (Bonus)
+# #  Exercie 5 (Bonus)
 
-string = "awesome sauce"
-vowels = "aeiou"
-print({vowel:string.count(vowel) for vowel in vowels })
+# string = "awesome sauce"
+# vowels = "aeiou"
+# print({vowel:string.count(vowel) for vowel in vowels })
+
+print('''       ___iiiii___
+      |:H:a:p:p:y:|
+    __|___________|__
+   |^^^^^^^^^^^^^^^^^|
+   |:B:i:r:t:h:d:a:y:|
+   |                 |
+   ~~~~~~~~~~~~~~~~~~~ ''')
